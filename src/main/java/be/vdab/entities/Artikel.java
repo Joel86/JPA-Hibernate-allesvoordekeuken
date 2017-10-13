bbd30c77bc3d2a46709ad995299a21d7945b22ce
Joel Vanherle <joel.vanherle@hotmail.com>
2017-10-13 14:03:52 +0200

    Added artikel entity class

---
+++ src/main/java/be/vdab/entities/Artikel.java
@@ -0,0 +1,39 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "artikels")
+public class Artikel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long id;
+	private String naam;
+	private BigDecimal aankoopprijs;
+	private BigDecimal verkoopprijs;
+	protected Artikel() {}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public BigDecimal getAankoopprijs() {
+		return aankoopprijs;
+	}
+	public void setAankoopprijs(BigDecimal aankoopprijs) {
+		this.aankoopprijs = aankoopprijs;
+	}
+	public BigDecimal getVerkoopprijs() {
+		return verkoopprijs;
+	}
+	public void setVerkoopprijs(BigDecimal verkoopprijs) {
+		this.verkoopprijs = verkoopprijs;
+	}
+	
+}
